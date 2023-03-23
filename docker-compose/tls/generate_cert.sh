2ccc37739352eeb4a8f16ad3256877ed43896444
RRoundTable <roundtable@annotation-ai.com>
2023-03-23 14:30:03 +0900

    add generate cert.sh

---
+++ docker-compose/tls/generate_cert.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Generate a self-signed certificate with subjectAltName (SAN).
+openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
+  -keyout certificates/whoami.local.key \
+  -out certificates/whoami.local.crt \
+  -subj "/CN=whoami.local" \
+  -addext "subjectAltName=DNS:whoami.local" \
+  -addext "extendedKeyUsage=serverAuth"
