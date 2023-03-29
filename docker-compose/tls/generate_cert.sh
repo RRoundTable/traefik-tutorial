0a7645ef730458419c6b81941e34463f2ca84cc6
RRoundTable <roundtable@annotation-ai.com>
2023-03-29 17:36:22 +0900

    Add mkdir for certificates

--- docker-compose/tls/generate_cert.sh
+++ docker-compose/tls/generate_cert.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Generate a self-signed certificate with subjectAltName (SAN).
+mkdir -p certificates
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
   -keyout certificates/whoami.local.key \
   -out certificates/whoami.local.crt \
